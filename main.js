271c679866c86c8e1ce3ff97788434d70aa8e877
Purin <purin.peera@mail.kmutt.ac.th>
2022-09-11 14:15:23 +0700

    js wk5

---
+++ main.js
@@ -0,0 +1,83 @@
+// let arr1=[1,3,5,7,9, true, 'Hello']
+// // console.log(arr1[0])
+// // console.log(arr1[arr1.length-1])
+// for(const arr of arr1){
+//     console.log(arr)
+// }
+
+// let building={buildingName:'LX'} //simple object
+// let colorObj={tone: 'warm color', colors:['red', 'orange']} //simple object
+// let colors=[['red', 'orange'],['blue', 'white', 'purple']] //nested array
+
+// console.log(colors[colors.length-1].length)
+// //colors[2-1].length //colors[1].length //3
+
+// let colorsObj=[
+//     {tone: 'warm color', colors:['red', 'orange']},
+//     {tone: 'pastel color', colors:['light green', 'grey']},
+//     {tone: 'cold color', colors:['blue', 'white', 'purple']} //object in arr
+// ]
+// console.log(colorsObj[1])
+// //object = {property key1: property value1, property key2, property value2}
+
+// let x=[]
+// let y=null
+// let z
+// console.log(x.length)
+// console.log(y?.length)
+// console.log(z)
+
+// let x=[1,3,5]
+// // let y=[...x, 7,9]
+// let y=Array.from(x)
+// y[y.length]=7
+// y[y.length]=9
+// console.log(x)
+// console.log(y)
+// let z=new Array(5,4)
+// console.log(z)
+// console.log(z.length)
+
+// let x=[1,3,5]
+// delete x[0]
+// console.log(x.length)
+// console.log(x)
+// delete x[1]
+// console.log(x.length)
+// console.log(x)
+// console.log(x[2])
+
+// let studentNames=['Sonchai', 'Somsri', 'Suda', 'Sakchai']
+// let[x,...names]=studentNames //destruction assignment //rest operator
+// // let x=studentNames[0]
+// // let y=studentNames[1]
+// // let z=studentNames[2]
+// console.log(x)
+// console.log(names)
+// // console.log(y)
+// // console.log(z)
+
+// let str='Hello'
+// let letters=[...str]
+// console.log(letters)
+// for (const [i,letter] of letters.entries()) {
+//     console.log(`index:${i}, value:${letter}`)
+// }
+
+//function declaring
+// function doSomething(x){
+//     return x
+// }
+// //execute function
+// console.log(doSomething(10))
+// let m=doSomething('Hello World')
+
+// let array1=['one', 'two', 'three'];
+// array1.reverse()
+// console.log('array:', array1);
+
+// const array2=[1,2,3]
+// array1=array1.concat(array2)
+// console.log(array1)
+// console.log(array2)
+
